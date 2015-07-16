9438317e39245b1a8ce5e53d920943819c100442
Jamel <jamelg310@gmail.com>
2015-07-16 16:59:23 -0700

    Finished twitter and chat

--- www/js/app.js
+++ www/js/app.js
@@ -3,27 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
-
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
     .run(function($ionicPlatform) {
       $ionicPlatform.ready(function() {
@@ -75,6 +55,26 @@ angular.module('starter', ['ionic'])
             }
           })
 
+          .state('tabs.tweet', {
+            url: '/tweet',
+            views: {
+              'tweet-tab':{
+                templateUrl: 'templates/tweet.html',
+                controller: 'tweetAuthCtrl'
+              }
+            }
+          })
+
+          .state('tabs.chats', {
+            url: '/chats',
+            views: {
+              'chats-tab': {
+                templateUrl: 'templates/chats.html',
+                controller: 'ChatsCtrl'
+              }
+            }
+          })
+
           .state('tabs.calendar', {
             url: "/calendar",
             views: {
@@ -84,6 +84,7 @@ angular.module('starter', ['ionic'])
               }
             }
           })
+
           .state('tabs.about', {
             url: "/about",
             views: {
@@ -146,4 +147,75 @@ angular.module('starter', ['ionic'])
           $scope.articles.splice($scope.articles.indexOf(item), 1);
         };
       });
-    }]);
+    }])
+
+.controller('tweetAuthCtrl', function($scope, $ionicPlatform, $twitterApi, $cordovaOauth) {
+
+    var twitterKey = 'STORAGE.TWITTER.KEY';
+    var clientId = '2uwmKcAXynJjt1NJdJep4KAtn';
+    var clientSecret = 'EB0h30qIZZCu8tPMewdhCxjJ3mmLehNKQyQpQW1aA2KohDWZy0';
+    var myToken = '';
+
+    $scope.tweet = {};
+
+    $ionicPlatform.ready(function() {
+      myToken = JSON.parse(window.localStorage.getItem(twitterKey));
+      if (myToken === '' || myToken === null) {
+        $cordovaOauth.twitter(clientId, clientSecret).then(function (succ) {
+          myToken = succ;
+          window.localStorage.setItem(twitterKey, JSON.stringify(succ));
+          $twitterApi.configure(clientId, clientSecret, succ);
+          $scope.showHomeTimeline();
+        }, function(error) {
+          console.log(error);
+        });
+      } else {
+        $twitterApi.configure(clientId, clientSecret, myToken);
+        $scope.showHomeTimeline();
+      }
+    });
+    $scope.showHomeTimeline = function() {
+      $twitterApi.getHomeTimeline().then(function(data) {
+        $scope.home_timeline = data;
+      });
+    };
+
+    $scope.submitTweet = function() {
+      $twitterApi.postStatusUpdate($scope.tweet.message).then(function(result) {
+        $scope.showHomeTimeline();
+      });
+    };
+
+    $scope.doRefresh = function() {
+      $scope.showHomeTimeline();
+      $scope.$broadcast('scroll.refreshComplete');
+    };
+
+    $scope.correctTimestring = function(string) {
+      return new Date(Date.parse(string));
+    };
+})
+
+.controller('ChatsCtrl', ['$scope', '$firebaseArray', '$rootScope',
+  function($scope, $firebaseArray, $rootScope) {
+    //grabbing firebase
+    var ref = new Firebase('https://dfs-chat.firebaseio.com/');
+    //grab firebase in a variable
+    //turn the object into a Array of items
+    $scope.chats = $firebaseArray(ref.child('chats'));
+
+    $scope.sendChat = function(chat){
+      //if($rootScope.authData) {
+      $scope.chats.$add({
+        //assigns users as guess
+        user: 'Guest',
+        //$rootScope.authData.twitter.username,
+
+        //gets all the things from the chat
+        message: chat.message
+        //imgURL: $rootScope.authData.twitter.cachedUserProfile.profile_image_url
+      });
+      chat.message = "";
+      //}
+    }
+  }]);
